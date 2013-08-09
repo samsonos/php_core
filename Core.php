89a12e5eeb67f5280cdf32f2d028b25728ed4072
vitalyiegorov <egorov@samsonos.com>
2013-08-09 18:49:55 +0300

    Minor changes

--- Core.php
+++ Core.php
@@ -1,4 +1,11 @@
-<?php 
+<?php
+/*
+ * This file is part of the SamsonPHP\Core package.
+ * (c) 20013 Vitaly Iegorov <egorov@samsonos.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace samson\core;
 
 /**
