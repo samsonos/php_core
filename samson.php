858bce50826c32f27a1484b56701ce058e813e47
Vitaly Iegorov <egorov@samsonos.com>
2014-03-06 12:26:35 +0200

    Added $function parameter to vdate, for using custom date renderer

--- samson.php
+++ samson.php
@@ -338,14 +338,15 @@ function vimg( $src, $id='', $class='', $alt = '', $dummy = null )
  *
  * @param string $name 	 Module view variable name
  * @param string $format Date format string
+ * @param string $function Function callback to render date
  */
-function vdate( $name, $format = 'h:i d.m.y' )
+function vdate( $name, $format = 'h:i d.m.y', $function = 'date' )
 {
 	// Cache current module
 	$m = & m();
 
 	// If view variable is set - echo with format
-	if ( $m->offsetExists( $name )) echo date( $format, strtotime($m[ $name ]));
+	if ( $m->offsetExists( $name )) echo $function( $format, strtotime($m[ $name ]));
 	
 }
 
