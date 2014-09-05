00ef362386518d0c1809bd612c64bc004bfc62af
Vitaly Iegorov <egorov@samsonos.com>
2014-09-05 09:54:20 +0300

    Updated default template worker to include conditional html5shiv and respond.js to support old browsers from CDN

--- src/Core.php
+++ src/Core.php
@@ -440,7 +440,8 @@ class Core implements iCore
 		
 		// Добавим поддержку HTML для старых IE
 		$head_html .= "\n".'<!--[if lt IE 9]>'; 
-		$head_html .= "\n".'<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>';
+		$head_html .= "\n".'<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>';
+        $head_html .= "\n".'<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>';
 		$head_html .= "\n".'<![endif]-->';
 			
 		// Выполним вставку главного тега <base> от которого зависят все ссылки документа
@@ -489,7 +490,7 @@ class Core implements iCore
 	/**	@see iCore::start() */
 	public function start( $default )
 	{
-        // Fire an core started event
+        // Fire core started event
         Event::fire('core.start');
 
 		// Parse URL
@@ -507,19 +508,6 @@ class Core implements iCore
 
         // Fire an core config ready event
         Event::fire('core.config_ready');
-
-
-		// Проинициализируем НЕ ЛОКАЛЬНЫЕ модули
-		/*foreach ($this->module_stack as $id => $module ) {
-            /** @var $module Module */
-
-			// Только внешние модули и их оригиналы
-			/*if (method_exists($module, 'init') && $module->id() == $id) {
-				//elapsed('Start - Initializing module: '.$id);
-				$module->init();
-				//elapsed('End - Initializing module: '.$id);
-			}
-		}*/
 		
 		// Результат выполнения действия модуля
 		$module_loaded = A_FAILED;
