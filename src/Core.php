2cf3d70003bf930d9aa98451878c6abefaae7e9b
Vitaly Iegorov <vitalyiegorov@gmail.com>
2014-10-17 14:34:06 +0300

    Returned last commit

--- src/Core.php
+++ src/Core.php
@@ -411,7 +411,7 @@ class Core implements iCore
 	{
 		// TODO: Change ExternalModule::init() signature
         // Fire core started event
-        Event::fire('core.started', array(array(&$this)));
+        Event::fire('core.started', array(&$this));
 
         // TODO: Does not see why it should be here
         // Set main template path
