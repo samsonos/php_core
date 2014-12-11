c7e1c473615910dacd4ed480e3fdb934180db24a
Vitaly Egorov <egorov@samsonos.com>
2014-12-11 11:54:00 +0200

    Added IConfigurable interface - first step in simplifying Module

---
+++ src/IConfigurable.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egorov
+ * Date: 11.12.2014
+ * Time: 11:50
+ */
+
+namespace samson\core;
+
+/**
+ * Interface for giving ability to class for generic configuration
+ * @package samson\core
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ */
+interface IConfigurable
+{
+    /**
+     * Perform instance configuration
+     * @param array $params Collection of parameters for configuration
+     * @return boolean True if class has been successfully configured
+     */
+    public function configure(array $params = array());
+}
