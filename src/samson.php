55efe5c266d5984518bf09fc3b2f13d3a76c4b1a
Vitaly Egorov <egorov@samsonos.com>
2015-05-13 13:11:36 +0300

    Fixed CLI run, disabled default usual web run

--- src/samson.php
+++ src/samson.php
@@ -6,34 +6,40 @@
  * @version 0.1.0
  */
 
+/**
+ * Do not start SamsonPHP framework if this is script is ran from CLI,
+ * because this is usually tests and system commands runs
+ */
+if (php_sapi_name() !== 'cli') {
 // Константы фреймворка SamsonPHP
-require('constants.php');
+    require('constants.php');
 
 // Set default timezone
-date_default_timezone_set(date_default_timezone_get());
+    date_default_timezone_set(date_default_timezone_get());
 
 // Set default execution time limit
-set_time_limit( __SAMSON_MAX_EXECUTION__ );
-ini_set('max_execution_time', __SAMSON_MAX_EXECUTION__);
+    set_time_limit(__SAMSON_MAX_EXECUTION__);
+    ini_set('max_execution_time', __SAMSON_MAX_EXECUTION__);
 
 // Start session
-session_start();
+    session_start();
 
 // Remove unnecessary files umask
-$old_umask = umask(0);
+    $old_umask = umask(0);
 
 // Include SamsonPHP auto loader
-require('AutoLoader.php');
+    require('AutoLoader.php');
 
-require('Config.php');
+    require('Config.php');
 
 
 // Load files with functions
-require('shortcuts.php');
-require('Utils2.php');
-require('View.php');
-require('deprecated.php');
+    require('shortcuts.php');
+    require('Utils2.php');
+    require('View.php');
+    require('deprecated.php');
 
 //[PHPCOMPRESSOR(remove,start)]
-require('Benchmark.php');
+    require('Benchmark.php');
 //[PHPCOMPRESSOR(remove,end)]
+}
