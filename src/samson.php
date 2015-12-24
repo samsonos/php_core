aece4fed2b5d74eb349af20ac4d4dddd79a4b838
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-12-24 17:40:26 +0200

    Removed Benchmark and it require

--- src/samson.php
+++ src/samson.php
@@ -11,39 +11,25 @@
  * because this is usually tests and system commands runs
  */
 if (php_sapi_name() !== 'cli') {
-// Константы фреймворка SamsonPHP
+    // Константы фреймворка SamsonPHP
     require('constants.php');
 
-// Set default timezone
+    // Set default timezone
     date_default_timezone_set(date_default_timezone_get());
 
-// Set default execution time limit
+    // Set default execution time limit
     set_time_limit(__SAMSON_MAX_EXECUTION__);
     ini_set('max_execution_time', __SAMSON_MAX_EXECUTION__);
 
-// Start session
+    // Start session
     session_start();
 
-// Remove unnecessary files umask
+    // Remove unnecessary files umask
     $old_umask = umask(0);
 
-// Include SamsonPHP auto loader
     require('AutoLoader.php');
-
-    require('Config.php');
-
-    // Load files with functions
     require('Utils2.php');
-
     require('shortcuts.php');
-
     require('View.php');
-
     require('deprecated.php');
-
-
-
-//[PHPCOMPRESSOR(remove,start)]
-    require('Benchmark.php');
-//[PHPCOMPRESSOR(remove,end)]
 }
