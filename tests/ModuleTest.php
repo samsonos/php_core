7c5d160bcbcafbd34e4392fdfc6fc77a24650e21
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-01-12 14:57:51 +0200

    Added module test Remove s() from module

--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -21,19 +21,14 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
     public function testView()
     {
         $path = __DIR__;
-        $ts = microtime(true);
+
         // Build resources map
         $map = ResourceMap::get($path);
-        trace(microtime(true) - $ts);
         // Create core
         $core = new Core($map);
-        trace(microtime(true) - $ts);
         // Create module
         $module = new Module('test', $path, $map, $core);
-        trace(microtime(true) - $ts);
-        $output = $module->view('path/inner/index')->output();
-        trace(microtime(true) - $ts);
 
-        $this->assertEquals('<h1>Test</h1>', $output);
+        $this->assertEquals('<h1>Test</h1>', $module->view('path/inner/index')->output());
     }
 }
