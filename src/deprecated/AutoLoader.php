b54d3e4547abd90fbf4188574178eed171759afa
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-01-20 11:54:19 -0400

    Moved Autoloader to deprecated

--- src/deprecated/AutoLoader.php
+++ src/deprecated/AutoLoader.php
@@ -224,7 +224,7 @@ class AutoLoader
         return false;
     }
 }
-// TODO: This class must be depreated and removed
+// TODO: This class must be deprecated and removed
 //[PHPCOMPRESSOR(remove,start)]
 // Add SamsonPHP default autoloader
 spl_autoload_register(array('\samson\core\AutoLoader','load'));
