05a9f4d1c05273fc87c28c9f91c4a71654386910
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-07-31 16:41:39 +0300

    Removed deprecated classes Removed constants and utils Removed max execution time

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -17,22 +17,9 @@ if (php_sapi_name() !== 'cli') {
     // Set default timezone
     date_default_timezone_set(date_default_timezone_get());
 
-    // Set default execution time limit
-    set_time_limit(__SAMSON_MAX_EXECUTION__);
-    ini_set('max_execution_time', __SAMSON_MAX_EXECUTION__);
-
     // Start session
     session_start();
 
     // Remove unnecessary files umask
     $old_umask = umask(0);
-
-    require('Utils2.php');
-    require('shortcuts.php');
-    require('View.php');
-
-    // deprecated
-    require('deprecated/AutoLoader.php');
-    require('deprecated/constants.php');
-    require('deprecated/Error.php');
 }
