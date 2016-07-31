78086d6c045c22fd87720f2cbf20037f12302a0a
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-01 00:13:27 +0300

    Refactored

--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -52,10 +52,13 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
     {
         $changedEnvironment = 'changedTest';
         $environment = 'test';
+
         Event::subscribe(Core::E_ENVIRONMENT, function(&$core, &$environment) use ($changedEnvironment) {
             $environment = $changedEnvironment;
         });
+
         $this->core->environment($environment);
+
         $this->assertEquals($changedEnvironment, $this->getProperty('environment', $this->core));
     }
     
@@ -77,13 +80,16 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
         $changedAlias = 'changedAlias';
         $moduleAlias = 'alias';
         $loadedAlias = '';
+
         Event::subscribe(Core::E_AFTER_LOADED, function(&$core, &$module, &$alias) use (&$loadedAlias) {
             $loadedAlias = $alias;
         });
         Event::subscribe(Core::E_BEFORE_LOADED, function(&$core, &$module, &$alias) use ($changedAlias) {
             $alias = $changedAlias;
         });
+
         $this->core->load(new TestModule(), $moduleAlias);
+
         $this->assertArrayHasKey($changedAlias, $this->getProperty('modules', $this->core));
         $this->assertEquals($changedAlias, $loadedAlias);
     }
